31680a7f45b2d868679b10290f42171288f62cf4
DhanrajMV <86152041+DhanrajMV@users.noreply.github.com>
2022-08-03 17:04:08 +0530

    added Demo Project
    added Demo Project

---
+++ DemoProject/src/test/java/SampleDemo.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+public class SampleDemo {
+	
+	@Test
+	public static void main() {
+		int a = 10;
+		int b = 20;
+		int c = a+b;
+		System.out.println(c);
+
+	}
+
+}
